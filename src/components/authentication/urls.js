4ef5bdee4033f03961ce446f13df106d1c2c475b
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2024-10-10 13:00:48 +0530

    v10101

--- src/components/authentication/urls.js
+++ src/components/authentication/urls.js
@@ -1,6 +1,6 @@
 
 export const urls = {
-    BASE_URL : "https://socialclubsbe.onrender.com/api",//DEVELOPMENT URL
-    // BASE_URL : "https://socialclubs.herokuapp.com/api",//PRODUCTION URL
+    // BASE_URL : "https://socialclubsbe.onrender.com/api",//DEVELOPMENT URL
+    BASE_URL : "http://127.0.0.1:8000/api",//PRODUCTION URL
 };
 
